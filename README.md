25eb63320a5f5eea765402423e2006455c37b847
Dani <danisotacam@gmail.com>
2023-12-30 03:14:21 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+Instalar Anaconda.
+Crear entorno con Python 10 (11 y 12 no valen)
+pip install gymnasium
+pip install stable-baselines3[extra]
+
+
+Copiar mi carpeta de gym-tanks.
+En el setup cambiar: gymnasium==0.29.1
+
+
+Ejecutar "pip install ."
+
+
+
+Problemas? --> necesitas Visual Studio, con cargas de trabajo:
+- Desarrollo para el escritorio con C++
+- Desarrollo de la plataforma universal de Windows
+- Desarrollo de Python
+- Linux y desarrollo integrado con C++
+también tengo preo creo que no hace falta:
+- Desarrollo de extensiones de Visual Studio y
+- Desarrollo de juegos con C++
+
+Componentes individuales:
+- C++ Build Insights
+- Windows 10 SDK
+- Herramientas de compilación de MSVC v142 -VS 2019 C++ para x64/x86 (v14.29-16.11)
+- Bibliotecas con mitigaciones de Spectre de MSVC ... igual q antes
+
+
+
+Ahora sí, Ejecutar "pip install ."
---
+++ agent_train.py
@@ -0,0 +1,55 @@
+# Import required libraries
+import gym_tanks
+import gymnasium as gym
+import os
+from stable_baselines3 import PPO
+
+# Define the test name for this particular run
+test_name = "Test_Kill"
+
+# Set the models and log folders
+models_dir = f"models/PPO/{test_name}"
+logdir = f"logs/{test_name}" # Run "tensorboard --logdir=logs" for plotting graphs
+
+TIMESTEPS = 10000 # Steps used by default by PPO
+
+# Path to the .zip file with pre-trained weights
+start_steps = 320000
+if start_steps > 0 and start_steps % TIMESTEPS == 0:
+    weights_path = f"models/PPO/{test_name}/model_{start_steps}_steps.zip"
+else:
+    # Train from zero
+    weights_path = None
+
+# Create folders if they don't exist
+os.makedirs(models_dir, exist_ok = True)
+os.makedirs(logdir, exist_ok = True)
+
+# Load and reset the environment
+env = gym.make('gym_tanks/tanks-v0')
+env.reset()
+
+# Check if the weights file exists
+if weights_path is not None:
+    model = PPO.load(weights_path, env = env, tensorboard_log = logdir, n_steps = TIMESTEPS)
+else:
+    # Create the model with PPO algorithm
+    model = PPO("MultiInputPolicy", env, verbose = 1, tensorboard_log = logdir, n_steps = TIMESTEPS)
+
+# Training parameters
+SAVE_INTERVAL = 10000 # Number of iterations to save
+TOTAL_TRAINING_TIMESTEPS = 1000000 # Total number of iterations for training
+
+total_timesteps = start_steps
+last_save = start_steps # Keep track of the last save point
+
+while total_timesteps < TOTAL_TRAINING_TIMESTEPS:
+    model.learn(total_timesteps = TIMESTEPS, reset_num_timesteps = False, tb_log_name = "PPO")
+    total_timesteps += TIMESTEPS
+    
+    # Check if it's time to save
+    if total_timesteps // SAVE_INTERVAL > last_save // SAVE_INTERVAL:
+        model.save(f"{models_dir}/model_{total_timesteps}_steps")
+        last_save = total_timesteps # Update the last save point
+
+env.close()
