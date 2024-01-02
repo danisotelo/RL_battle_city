d57e48a1e0def4e3e8720d34d053c964cf623d4f
Dani <danisotacam@gmail.com>
2024-01-02 12:50:26 +0100

    Change maximum episodes

--- gym_tanks/__init__.py
+++ gym_tanks/__init__.py
@@ -3,5 +3,5 @@ from gymnasium.envs.registration import register
 register(
      id="gym_tanks/tanks-v0",
      entry_point="gym_tanks.envs:TanksEnv",
-     max_episode_steps=300,
+     max_episode_steps=3000,
 )
